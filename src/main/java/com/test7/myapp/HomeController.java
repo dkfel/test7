a3e2f9eeeec96a1937ce827f6b29303d2a678292
admin <admin@302PC-004>
2021-04-08 18:01:31 +0900

    System.out.println("준성 JUN_FILE_TEST");

--- src/main/java/com/test7/myapp/HomeController.java
+++ src/main/java/com/test7/myapp/HomeController.java
@@ -38,6 +38,8 @@ public class HomeController {
 		System.out.println("霖己 test3");
 		System.out.println("霖己 test4");
 		System.out.println("霖己 test0408");
+		System.out.println("霖己 test0408");
+		System.out.println("霖己 JUN_FILE_TEST");
 		return "home";
 		
 		
