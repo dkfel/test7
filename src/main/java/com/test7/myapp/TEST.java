fb674ec5d112e15d095000cf94bfc0977a30c37f
admin <admin@302PC-004>
2021-04-08 18:27:59 +0900

    import test 소스 수정

--- src/main/java/com/test7/myapp/TEST.java
+++ src/main/java/com/test7/myapp/TEST.java
@@ -3,18 +3,15 @@ package com.test7.myapp;
 public class TEST {
 
 	public static void main(String[] args) {
-		System.out.println("테스트입니다");
-		System.out.println("테스트용 하나더");
-		System.out.println("테스트11");System.out.println("테스트11");
-		System.out.println("테스트112232321");
-		System.out.println("테스트12312123232");
-		System.out.println("테스트1112123213");
+		System.out.println("�뀒�뒪�듃�엯�땲�떎");
+		System.out.println("�뀒�뒪�듃�슜 �븯�굹�뜑");
+		System.out.println("�뀒�뒪�듃11");System.out.println("�뀒�뒪�듃11");
+		System.out.println("�뀒�뒪�듃112232321");
+		System.out.println("�뀒�뒪�듃12312123232");
+		System.out.println("�뀒�뒪�듃1112123213");
 		
-		System.out.println("테스트11");
-		System.out.println("테스트11");
-		System.out.println("테스트11");
-		System.out.println("테스트11");
-		System.out.println("테스트11");
+		System.out.println("�뀒�뒪�듃11");
+		System.out.println("�뀒�뒪�듃11");
 		
 		
 		
